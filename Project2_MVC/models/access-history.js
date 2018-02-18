689e021278352b6ac234aedf2ebaa70058bc0e67
Ondrej Tomco <ondrej.tomco@akademiasovy.sk>
2018-02-18 20:06:55 +0100

    access-history schema added toi database

---
+++ Project2_MVC/models/access-history.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://Admin1:akademiasovy@ds229388.mlab.com:29388/recog');
+
+const accessHistorySchema = new Schema({
+    Access_time: {
+        type:Date,
+        default: Date.now
+    },
+    Employee_id: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'Employee'
+    },
+    Success: {
+        type:boolean
+    }
+    
+
+});
+
+module.exports = mongoose.model('AccesHistory',accessHistorySchema);
