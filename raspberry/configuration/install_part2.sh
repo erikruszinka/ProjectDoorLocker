930c4c4a98b661bd24b699d98a5b94b9a829b14e
michal.novotny <michal.novotny@akademiasovy.sk>
2018-02-28 13:37:14 +0100

    preparing install scripts

---
+++ raspberry/configuration/install_part2.sh
@@ -0,0 +1,14 @@
+sudo apt-get purge wolfram-engine
+sudo apt-get purge libreoffice*
+sudo apt-get clean
+sudo apt-get autoremove
+sudo apt-get install python-dev python~rpi.gpio
+sudo apt-get install python2.7-dev python3-dev
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk2.0-dev libgtk-3-dev
+sudo apt update
+sudo apt upgrade
+reboot
