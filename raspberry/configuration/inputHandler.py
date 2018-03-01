a2c777c8e49701970bdae0964f7e0f64b4b1063c
michal.novotny <michal.novotny@akademiasovy.sk>
2018-03-01 14:40:24 +0100

    Input handler added

---
+++ raspberry/configuration/inputHandler.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+import os
+pinShutdown = 26
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pinShutdown,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+pinTestButton = 21
+GPIO.setup(pinTestButton,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+os.system("")
+
+while True:
+    input_state_shutdown = GPIO.input(pinShutdown)
+    input_state_testPin = GPIO.input(pinTestButton)
+    if input_state_shutdown == False:
+        print('pressed Shutdown')
+        os.system("shutdown -h now")
+    elif input_state_testPin == False:
+        print('pressed Test')
+    else:
+        time.sleep(1)
