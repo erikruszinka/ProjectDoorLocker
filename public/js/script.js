0c0693378876ecccd5cddff2591475d5f4123bf7
Erik Ruszinka <erik.ruszinka@gmail.com>
2018-03-06 15:24:56 +0200

    tabs of access points

--- public/js/script.js
+++ public/js/script.js
@@ -8,4 +8,33 @@ $(document).ready(function(){
           $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
       });
+
+      
+
 })
+
+document.getElementById("defaultOpen").click();
+function openMe(inside) {
+    var i, content;
+    content = document.getElementsByClassName("content");
+    for (i=0; i<content.length;i++)
+        {
+         content[i].style.display="none";
+        }
+    document.getElementById(inside).style.display="block";
+  
+}
+
+//$(".button").click(function(){
+    //$(this).toggleClass("act");
+    
+  //});
+  var header = document.getElementById("tabs");
+  var btns = header.getElementsByClassName("tab");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
